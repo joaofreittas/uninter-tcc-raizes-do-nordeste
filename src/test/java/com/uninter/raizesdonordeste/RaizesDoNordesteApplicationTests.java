08b5318c4a4975c743cf4d9559d4957b5cab2b99
joaofreittas <joao.frhb@gmail.com>
2026-06-14 20:09:59 -0300

    feat: primeiro commit estrutura proojeto

---
+++ src/test/java/com/uninter/raizesdonordeste/RaizesDoNordesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uninter.raizesdonordeste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaizesDoNordesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
