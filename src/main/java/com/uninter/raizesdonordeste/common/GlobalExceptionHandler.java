528f6cc464ae4bb7372cb9b4e2a0b9cde7d0fad9
joaofreittas <joao.frhb@gmail.com>
2026-06-21 13:35:06 -0300

    fix: ajustando status de retorno em caso de não autorização

--- src/main/java/com/uninter/raizesdonordeste/common/GlobalExceptionHandler.java
+++ src/main/java/com/uninter/raizesdonordeste/common/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -52,6 +53,14 @@ public class GlobalExceptionHandler {
         return ResponseEntity
             .status(HttpStatus.BAD_REQUEST)
             .body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), message));
+    }
+
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    public ResponseEntity<ErrorResponse> handleAuthorizationDenied(AuthorizationDeniedException ex) {
+        logError(ex);
+        return ResponseEntity
+            .status(HttpStatus.FORBIDDEN)
+            .body(new ErrorResponse(HttpStatus.FORBIDDEN.value(), "Acesso negado"));
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
